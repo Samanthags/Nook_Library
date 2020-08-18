425cf7960b63841c9d4738fca9336d764a91eecc
Samanthags <samanthags3644@gmail.com>
2020-08-18 00:10:56 -0400

    cart items update

--- app.js
+++ app.js
@@ -169,6 +169,25 @@ div.innerHTML = `<img src=${item.image} alt="product"/>
                 let id = addAmount.dataset.id;
                 let tempItem = cart.find(item => item.id===id);
                 tempItem.amount = tempItem.amount + 1;
+                Storage.saveCart(cart);
+                this.setCartValues(cart);
+                addAmount.nextElementSibling.innerText = tempItem.amount;
+            }
+            else if(event.target.classList.contains("fa-chevron-down")){
+                let lowerAmount = event.target;
+                let id = lowerAmount.dataset.id;
+                let tempItem = cart.find(item => item.id===id);
+                tempItem.amount = tempItem.amount - 1;
+                if(tempItem.amount > 0){
+                    Storage.saveCart(cart);
+                    this.setCartValues(cart);
+                    addAmount.previousElementSibling.innerText = tempItem.amount;
+                }
+                    else{
+                        cartContent.removeChild(lowerAmount.parentElement.parentElement);
+                        this.removeItem(id)
+                    } 
+
             }
         });
     }
